7b7eab29962e7aa94e85f9730f6d104f4aae4611
AleGab21 <alex_gabrielsen@hotmail.com>
2023-09-14 16:04:45 +0200

    dewletes build folders

--- .arduino/output/core/WInterrupts.c.d
+++
@@ -1,6 +0,0 @@
-c:\Users\alex_\OneDrive\Skrivebord\NTNU\IELS\Software\Arduino-Test\.arduino\output\core\WInterrupts.c.o: \
- C:\Users\alex_\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\WInterrupts.c \
- C:\Users\alex_\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\wiring_private.h \
- C:\Users\alex_\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\Arduino.h \
- C:\Users\alex_\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\binary.h \
- C:\Users\alex_\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard/pins_arduino.h
--- .arduino/output/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-c:\Users\alex_\OneDrive\Skrivebord\NTNU\IELS\Software\Arduino-Test\.arduino\output\core\WString.cpp.o: \
- C:\Users\alex_\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\WString.cpp \
- C:\Users\alex_\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\WString.h
