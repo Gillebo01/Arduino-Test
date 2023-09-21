28f394fa70c587c63c3795e4772237de2fee4c8a
AleGab21 <alex_gabrielsen@hotmail.com>
2023-09-21 14:52:27 +0200

    add prosjekt ledlist

---
+++ Ledlist uke 38/potentio/potentio.ino
@@ -0,0 +1,12 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 3           // Pin for LED-stripe
+#define NUM_LEDS 32          // Antall LED-er i stripen
+#define POTENTIOMETER_PIN A1 // Analog pin for potensiometer
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+
+
+
+ 
